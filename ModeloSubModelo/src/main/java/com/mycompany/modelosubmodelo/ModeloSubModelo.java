c0e27f7e2f6f2625bd7725b348a67eaf9bf33e37
alumno <alumno@DESKTOP-C1P4AJ1>
2024-06-30 13:41:46 -0400

    Probando...

---
+++ ModeloSubModelo/src/main/java/com/mycompany/modelosubmodelo/ModeloSubModelo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.modelosubmodelo;
+
+/**
+ *
+ * @author alumno
+ */
+public class ModeloSubModelo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hola mundo");
+    }
+}
